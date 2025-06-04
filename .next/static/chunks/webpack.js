6b7f8358d464e25e38d0ff59264b597789a9154d
Maryam <maryam_parvaz@yahoo.com>
2025-06-04 12:57:19 +0330

    Save current changes before pull

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "095bd6f698a30037"; }
+/******/ 		__webpack_require__.h = function() { return "b817456352cec44a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
